655f00c1c6f21d629a113906ec4233906e4d2c83
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-04-09 14:41:07 +0300

    feat: add page-up btn

--- run-smart/src/js/script.js
+++ run-smart/src/js/script.js
@@ -120,21 +120,15 @@ $(document).ready(function () {
     return false;
   });
 
-//   $('form').submit(function(e) {
-//     e.preventDefault();
-//     $.ajax({
-//         type: "POST",
-//         url: "mailer/smart.php",
-//         data: $(this).serialize()
-//     }).done(function() {
-//         $(this).find("input").val("");
-//         $('#consultation, #order').fadeOut();
-//         $('.overlay, #thanks').fadeIn('slow');
-//         $('form').trigger('reset');
-//     });
-//     return false;
-// });
+  //smooth scroll
 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
 });
 
 // const slider = tns({
