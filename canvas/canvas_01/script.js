ae5e7459fbc0784696d75655fa5199b19107d1e6
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-21 16:29:48 +0300

    feat: canvas canvas_01

---
+++ canvas/canvas_01/script.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('#c1');
+const ctx = canvas.getContext('2d');
+
+// ctx.fillRect(x, y, w, h);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(100, 50, 150, 75);
+ctx.fillStyle = 'blue';
+ctx.fillRect(150, 100, 100, 50);
+
+ctx.clearRect(0, 0, 400, 200);
+
+ctx.strokeStyle = 'green';
+ctx.lineWidth = 10;
+ctx.rect(50, 10, 100, 100);
+ctx.stroke();
+ctx.fillStyle = 'orange';
+ctx.fill();
